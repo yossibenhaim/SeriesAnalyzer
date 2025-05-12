c49af0e56e988a307592c72932857cfea2070393
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-12 15:54:17 +0300

    yossi#added_all_names_of_functions

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -8,8 +8,105 @@ namespace ConsoleApp8
 {
     internal class Program
     {
+        static bool inputValidation3Int()
+        {
+
+        }
+
+        static List<string> insertingInput()
+        {
+
+        }
+
+        static void printInOrder()
+        {
+
+        }
+
+        static void printNotInOrder()
+        {
+
+        }
+
+        static void printSortList()
+        {
+
+        }
+
+        static void printBigItem()
+        {
+
+        }
+
+        static void printLowItem()
+        {
+
+        }
+
+
+        static void printAverageOfList()
+        {
+
+        }
+
+
+        static void printCountOfAlements()
+        {
+
+        }
+
+
+        static void printSumOfAllAlements()
+        {
+
+        }
+
+        static void menu(List<string> list)
+        {
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    list = insertingInput();
+                    break;
+                case 2:
+                    printInOrder();
+                    break;
+                case 3:
+                    printNotInOrder();
+                    break;
+                case 4:
+                    printSortList();
+                    break;
+                case 5:
+                    printBigItem();
+                    break;
+                case 6:
+                    printLowItem();
+                    break;
+                case 7:
+                    printAverageOfList();
+                    break;
+                case 8:
+                    printCountOfAlements();
+                    break;
+                case 9:
+                    printSumOfAllAlements();
+                    break;
+                case 10:
+                    printSortList();
+                    break;
+            }
+        }
+
+
+
+
         static void Main(string[] args)
         {
+
+
+
+
         }
     }
 }
